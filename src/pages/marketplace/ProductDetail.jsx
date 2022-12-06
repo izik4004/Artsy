af4f96f1ff6cfe7df08ac6f4c6de442dc86bdf53
izik <izik4004@gmail.com>
2022-12-06 21:04:58 +0100

    collections section

--- src/pages/marketplace/ProductDetail.jsx
+++ src/pages/marketplace/ProductDetail.jsx
@@ -1,4 +1,9 @@
 import {BsSuitHeart} from "react-icons/bs"
+import {RiSubtractFill} from "react-icons/ri"
+import {AiOutlinePlus} from "react-icons/ai"
+import {IoIosArrowDown} from "react-icons/io"
+import {MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight} from "react-icons/md"
+import image from "../../assets/productDetail.png"
 
 export const ProductDetail = () => {
   return (
@@ -6,30 +11,61 @@ export const ProductDetail = () => {
         <div className='my-14'>hi</div>
         <div className='flex border my-10'>
             <div className='border-r w-1/2'>
-                <img src="" alt="" />
-                <h1>lkkljkldlkdlsdkl</h1>
+                <img src={image} alt="" className="px-4 py-10"/>
             </div>
             <div className='flex flex-col w-1/2 '>
-                <div className='border-b p-6 '>kkljdfljkdfjkldjkls</div>
-                <div className='border-b p-6'>
-                    <p>Creator:</p><span></span>
-                    <p>Made in Italy</p>
-                    <p>Total views::</p><span>1.7k views</span>
-                    <p className='flex gap-4'>
-                        <span>-</span>
-                        <span>1</span>
-                        <span>+</span>
-                    </p>
-                    <div className='flex'>
-                    <button className='px-6 py-2 bg-[#3341C1]'>Add to cart</button>
+                <div className='border-b py-14 px-6 flex justify-between items-center'>
+                    <h3 className="text-3xl font-semibold">Boolean Egyptian</h3>
                     <div>
-                        <BsSuitHeart />
-                    </div>
+                        <h5 className="text-lg">0.09</h5>
                     </div>
                 </div>
-
+                <div className='border-b px-6 py-12'>
+                    <p className="text-lg">Creator:</p><span></span>
+                    <p className="text-sm py-4">Made in Italy</p>
+                    <p className="text-md py-4">Total views:<span className="px-2">1.7k views</span></p>
+                    <p className='flex gap-4 py-6 items-center'>
+                        <RiSubtractFill className="text-2xl cursor-pointer"/>
+                        <span className="text-xl">1</span>
+                        <AiOutlinePlus className="text-xl cursor-pointer"/>
+                        
+                    </p>
+                    <div className='flex items-center gap-4 py-6'>
+                    <button className='px-14 py-4 bg-[#3341C1] text-white tracking-wider'>Add to cart</button>
+                    <div className="border px-6 py-4">
+                        <BsSuitHeart size={24}/>
+                        </div>
+                    </div>
+                </div>
+                <div className='border-b px-6 py-10 flex justify-between items-center cursor-pointer'>
+                    <h3 className="text-xl">Description</h3>
+                    <IoIosArrowDown size={24}/>
+                </div>
+                <div className='border-b px-6 py-10 flex justify-between items-center cursor-pointer'>
+                    <h3 className="text-xl">Description</h3>
+                    <IoIosArrowDown size={24}/>
+                </div>
+                <div className='border-b px-6 py-10 flex justify-between items-center cursor-pointer'>
+                    <h3 className="text-xl">Description</h3>
+                    <IoIosArrowDown size={24}/>
+                </div>
             </div>
+
+           
         </div>
+        <section className="my-20  py-4 px-4 rounded-lg shadow">
+                <div className="flex justify-between items-center">
+                    <h3>Explore more from this collections</h3>
+                    <div className="flex justify-between gap-6">
+                        <div className="border p-2 rounded-full">
+                        <MdOutlineKeyboardArrowLeft size={20}/>
+                        </div>
+                        <div className="border p-2 rounded-full">
+                            <MdOutlineKeyboardArrowRight size={20}/>
+                        </div>
+                    </div>
+                </div>
+            </section>
     </section>
   )
 }
