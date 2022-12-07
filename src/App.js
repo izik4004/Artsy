171e3497a030b1e42dbb67a3a2ff0bac8be5d262
izik <izik4004@gmail.com>
2022-12-07 22:24:33 +0100

    route refactoring

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
 import "./App.css";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 // import Header from './components/Header';
 import Nav from "./components/Nav";
 // import Newsletter from './components/Newsletter';
 import Homepage from "./pages/homepage/Index";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
 import { ProductDetail } from "./pages/marketplace/ProductDetail";
 import MarketPlace from "./pages/marketplace/MarketPlace";
 import PaymentCheckout from "./pages/checkout/PaymentCheckout";
+import ErrorPage from "./pages/ErrorPage";
+
+
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Nav/>
+      <Outlet/>
+      <Footer />
+    </div>
+  )
+}
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <Homepage/>,
+      },
+      {
+        path: "/marketplace",
+        element: <MarketPlace/>,
+      },
+      {
+        path: "/products/:id",
+        element: <ProductDetail/>,
+      },
+      {
+        path: "/checkout",
+        element: <PaymentCheckout/>,
+      },
+      {
+        path: "/",
+        // element: <ErrorPage />,
+        errorElement: <ErrorPage/>,
+      },
+    ]
+  },
+ 
+]);
+
 
 function App() {
   return (
-    <div className="">
-     
-      <Router>
-      <Nav />
-        <Routes>
-          <Route exact path="/" element={<Homepage />} />
-          <Route path="/product_detail" element={<ProductDetail />} />
-          <Route path="/marketplace" element={<MarketPlace/>} />
-          <Route path="/paymentcheckout" element={<PaymentCheckout/>} />
-        </Routes>
-      </Router>
-      
+    <div>
+     <RouterProvider router={router} />
     </div>
   );
 }
